d05ea996660f5011cd1af1ba04b26fbaf4488119
rodpru <rgp.prt@gmail.com>
2020-11-17 16:03:37 +0000

    login

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const session      = require('express-session'); 
 
 
 mongoose
@@ -36,6 +37,15 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+
+
+app.use(
+  session({
+    secret: 'my secret',
+    cookie: {maxAge: 60000}, //1min
+    rolling: true //--> makes the session on if you're browsing
+  })
+)
 
 
 
--- models/HelpType.js
+++ models/HelpType.js
@@ -13,7 +13,7 @@ const helpTypeSchema = new Schema ({
   subServices: {
     type: String,
     required: true,
-    enum: ['Doctor', 'Nurse', 'psychologist', 'Lawyer', 'Restaurant','Grocery Store','Individual','Landlord','test','test','test','test', 'other volunteer']
+    enum: ['Doctor', 'Nurse', 'Psychologist', 'Lawyer', 'Restaurant','Grocery Store','Individual','Landlord','test','test','test','test', 'other volunteer']
   }
 });
 
