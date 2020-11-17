af25336fc040813cb003487b2b52f64e25c6c65a
Salome-Cha <salome.charpignon@gmail.com>
2020-11-17 12:21:08 +0000

    "commit"

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -4,11 +4,21 @@ document.addEventListener('DOMContentLoaded', () => {
 
 }, false);
 
-//Map Initiation
-let map;
+//Helper Map Initiation
+let helperMap;
 function initMap() {
   const lisbon = { lat: 38.7117206, lng: -9.1264315 };
-  map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById('helperMap'), {
+    zoom: 17,
+    center: lisbon
+  })
+}
+
+//Needy Map Initiation
+let needyMap;
+function initMap() {
+  const lisbon = { lat: 38.7117206, lng: -9.1264315 };
+  map = new google.maps.Map(document.getElementById('needyMap'), {
     zoom: 17,
     center: lisbon
   })
