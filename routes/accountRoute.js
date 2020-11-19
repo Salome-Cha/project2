0c7dd71015d18ce2b984d78041ac4d9aa4671fc4
rodpru <rgp.prt@gmail.com>
2020-11-19 11:30:54 +0000

    user

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -48,6 +48,17 @@ router.get('/n-profile/:userId', (req, res) =>{
 
 //---------- EDIT THE PROFILE -------------
 
+router.get('/n-profile/:userId/edit', (req, res) => {
+  let userId = req.params.userId;
+  User.findById(userId)
+    .populate('inNeedType')
+      .then((thisUser) =>{ 
+        res.render('main/edit-needy-profile', {user: thisUser})
+       })
+       .catch((err) => {
+        res.render('error', {err}) 
+      })    
+});
 
 
 module.exports = router;
